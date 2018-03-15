b91324ddb4cb2d6ecf311bf825b316bd3037e4aa
roshan101 <roshanbudhathoki02@gmail.com>
2018-03-15 04:40:41 +0000

    Still error in in filter

--- webapp/controller/BookList.controller.js
+++ webapp/controller/BookList.controller.js
@@ -10,6 +10,10 @@ sap.ui.define([
 	return Controller.extend("sap.ui.demo.walkthrough.controller.BookList", {
 		formatter: formatter,
 		onInit: function(){
+			var oTable = this.byId("bookList");
+			this._oTable = oTable;
+			//keeps the search state
+			this._oTableSearchState = [];
 			var oViewModel = new JSONModel({
 				currency: "NPR",
 				countall: 0,
@@ -18,20 +22,51 @@ sap.ui.define([
 				soldout: 0
 			});
 			this.getView().setModel(oViewModel, "view");
-			
 			//create an object of filters
 			this._mFilters = {
-				"countall": [],
-				"bnew": [ new sap.ui.model.Filter("BooksInStock", "GT", 10)],
-				"instock": [ new sap.ui.model.Filter("BooksInStock", "GT", 10)],
-				"soldout": [ new sap.ui.model.Filter("BooksInStock", "BT", 1, 10)]
+				"bnew": [ new sap.ui.model.Filter("book>Status", "StartsWith", "N")],
+				"instock": [ new sap.ui.model.Filter("book>Status", "StartsWith","I" )],
+				"soldout": [ new sap.ui.model.Filter("book>Status", "StartsWith", "S")],
+				"countall": []
 			};
 		},
-		onUpdateFinished: function(){
-			var oViewModel = this.getModel("view");
-			
-			//Get the count for all the books and set the value to 'countall' property
-			
+		onUpdateFinished: function(oEvent){
+			var oViewModel = this.getView().getModel("view");
+				//Get the count for all the books and set the value to 'countall' property
+			if(oViewModel){
+				this.getView().getModel().read("/Books/$count", {
+		         success: function (oData) {
+		            oViewModel.setProperty("/countall", oData);
+		         }
+		      });
+		      // read the count for the New filter
+		      this.getModel().read("/Books/$count", {
+		         success: function (oData) {
+		            oViewModel.setProperty("/bnew", oData);
+		         },
+		         filters: this._mFilters.bnew
+		      });
+		      // read the count for the inStock filter
+		      this.getModel().read("/Books/$count", {
+		         success: function(oData){
+		            oViewModel.setProperty("/instock", oData);
+		         },
+		         filters: this._mFilters.instock
+		      });  
+		      // read the count for the soldout filter
+		      this.getModel().read("/Books/$count", { 
+		         success: function(oData){
+		            oViewModel.setProperty("/soldout", oData);
+		         },
+		         filters: this._mFilters.soldout
+		      }); 
+			}
+		},
+		onQuickFilter: function(oEvent){
+
+			var oBinding = this._oTable.getBinding("items"),
+			sKey = oEvent.getParameter("selectedKey");
+			oBinding.filter(this._mFilters[sKey]);
 		},
 		onFilterBooks: function (oEvent) {
 			//build filter array
